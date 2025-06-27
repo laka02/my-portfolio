1247d85bbec99b5873b6d9abbc5496ecd12f838c
laka02 <lakshikaviduranga20021017@gmail.com>
2025-06-27 14:36:34 +0530

    c

---
+++ js/script.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+    setTimeout(function() {
+        document.getElementById('loader-bg').style.display = 'none';
+    }, 2500); // Show loader for at least 2 seconds
+});
